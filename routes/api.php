36afc2a0929a8738f28e4daa14175932c8ddd489
feryalicization <fery.chaerul.ismail94@gmail.com>
2025-10-15 22:27:24 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -355,7 +355,7 @@ Route::middleware(['auth:api'])->group(function () {
 
 
 // check api
-Route::get('/health', function () {
+Route::get('/health-v2', function () {
     return response()->json([
         'status' => 'ok',
         'time' => now()->toDateTimeString(),

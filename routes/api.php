bcee9540e1b8068b699da8b9347d38f367c3ebf9
feryalicization <fery.chaerul.ismail94@gmail.com>
2025-10-16 11:01:30 +0700

    test cd ci 2

--- routes/api.php
+++ routes/api.php
@@ -355,7 +355,7 @@ Route::middleware(['auth:api'])->group(function () {
 
 
 // check api
-Route::get('/health-v1', function () {
+Route::get('/health-v2', function () {
     return response()->json([
         'status' => 'ok',
         'time' => now()->toDateTimeString(),

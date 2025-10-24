f72a3ec02bcb9574c51c528ee0ba557d51301259
feryalicization <fery.chaerul.ismail94@gmail.com>
2025-10-24 10:58:18 +0700

    add api risk profile investasi

---
+++ app/Models/TrRiskInvestasi.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TrRiskInvestasi extends Model
+{
+    protected $table = 'tr_risk_investasi';
+
+    protected $fillable = [
+        'erkap_id',
+        'kategori_risiko',
+        'sub_kategori_risiko',
+        'sasaran',
+        'peristiwa_risiko',
+        'penyebab_risiko',
+        'dampak_inherent',
+        'dampak_risiko_awal',
+        'kemungkinan_awal',
+        'eksposure_level_awal',
+        'eksposure_ltmh_awal',
+        'internal_external',
+        'mitigasi_risiko',
+        'dampak_residual',
+        'dampak_risiko_akhir',
+        'kemungkinan_akhir',
+        'eksposure_level_akhir',
+        'eksposure_ltmh_akhir',
+        'biaya_mitigasi_risiko',
+        'status',
+        'approval_notes',
+        'approved_by',
+        'approved_at',
+        'vp_menrisk_note',
+        'vp_menrisk_by',
+        'vp_menrisk_at',
+        'menrisk_note',
+        'menrisk_by',
+        'menrisk_at',
+        'created_by',
+        'updated_by'
+    ];
+
+    protected $casts = [
+        'peristiwa_risiko' => 'array',
+        'penyebab_risiko' => 'array',
+        'internal_external' => 'array',
+        'mitigasi_risiko' => 'array',
+        'approved_at' => 'datetime',
+        'vp_menrisk_at' => 'datetime',
+        'menrisk_at' => 'datetime',
+    ];
+
+    public function investasi()
+    {
+        return $this->belongsTo(\App\Models\RencanaInvestasi::class, 'erkap_id', 'id');
+    }
+
+
+    public function approvedByUser()
+    {
+        return $this->belongsTo(User::class, 'approved_by')->select(['id','name','username']);
+    }
+}

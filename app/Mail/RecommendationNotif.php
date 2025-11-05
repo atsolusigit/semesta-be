0d5777ca27501163d3785c459870a7e1fe4f1a71
Deo <kusnaedi.m@gmail.com>
2025-11-05 11:54:28 +0700

    Update Mail class untuk rekomendasi var

--- app/Mail/RecommendationNotif.php
+++ app/Mail/RecommendationNotif.php
@@ -13,22 +13,22 @@ class RecommendationNotif extends Mailable
 {
     use Queueable, SerializesModels;
 
-    // public $erkapID;
-    // public $namaInvestasi;
-    // public $tahun;
-    // public $rekomendasi;
-    public $data;
+    public $erkap_id;
+    public $nama_investasi;
+    public $tahun;
+    public $rekomendasi;
+    // public $data;
 
     /**
      * Create a new message instance.
      */
-    public function __construct($data)
+    public function __construct($erkap_id, $nama_investasi, $tahun, $rekomendasi)
     {
-        $this->data = $data;
-        // $this->erkapID = $erkapID;
-        // $this->namaInvestasi = $namaInvestasi;
-        // $this->tahun = $tahun;
-        // $this->rekomendasi = $rekomendasi;
+        // $this->data = $data;
+        $this->erkap_id = $erkap_id;
+        $this->nama_investasi = $nama_investasi;
+        $this->tahun = $tahun;
+        $this->rekomendasi = $rekomendasi;
     }
 
     /**
